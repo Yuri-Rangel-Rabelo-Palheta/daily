2a4f9d7ae46a3d7d9e4d6feabe3dade348aa7f6a
yuri.palheta <yuri.palheta@ufra.edu.br>
2024-12-13 23:21:20 -0300

    alteraçoes

--- src/lib/firebaseConfig.tsx
+++ src/lib/firebaseConfig.tsx
@@ -9,13 +9,21 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyD8dvfzew9E6zmRKOD6jgxS-zKnFLsdpXY",
+
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+
+/*   apiKey: "AIzaSyD8dvfzew9E6zmRKOD6jgxS-zKnFLsdpXY",
   authDomain: "daily-7345f.firebaseapp.com",
   projectId: "daily-7345f",
   storageBucket: "daily-7345f.firebasestorage.app",
   messagingSenderId: "298851233366",
   appId: "1:298851233366:web:147721781baf3c1f6795f7",
-  measurementId: "G-ZCR6G2PF69"
+  measurementId: "G-ZCR6G2PF69" */
 };
 
 // Initialize Firebase
